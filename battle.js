f41d5478fc7e87b6da0b8f7fe4ac2740195f4ef8
dogrizz <dogrizz@gmail.com>
2022-11-04 08:50:48 +0100

    Fixed player render

--- battle.js
+++ battle.js
@@ -113,7 +113,7 @@
         ship = vnode.attrs.ship
         fleet = vnode.attrs.fleet
 
-        return m('div', { class: 'column' + ship.owner !== fleet.name ? ' captured' : '' }, [
+        return m('div', { class: ship.owner !== fleet.name ? 'column captured' : 'column' }, [
           m('div', { class: 'row', style: 'gap: 5px' }, [
             m('h4', { class: ship.destroyed ? 'dead' : '' }, ship.name),
             m(
