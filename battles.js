bc371d3619f9224796313bc6cbd26812c08ff76e
Dominik Zmitrowicz <dogrizz@gmail.com>
2025-01-21 00:15:59 +0100

    fix battle list

--- battles.js
+++ battles.js
@@ -10,7 +10,7 @@
         m(OptionsComponent, { hideBattleLink: true }),
         m('main', { class: 'container' }, [
           m('h1', 'MF0 Intercept Orbit battles'),
-          m('div', { class: 'row' }, [
+          m('div', { class: 'battle list row row-cols-1 gap-3' }, [
             !battles || Object.entries(battles).length === 0
               ? [
                 m('span', 'No battles yet. Grab your bricks, dice and get to it!'),
@@ -19,13 +19,13 @@
               : Object.entries(battles).map(function(entry) {
                 const date = new Date(entry[1].date)
                 const id = entry[0]
-                return m('div', { class: 'col row align-items-center justify-content-start gap-2' }, [
+                return m('div', { class: 'battle col row align-items-center justify-content-start gap-2' }, [
                   m('span', { class: 'col fs-5' }, `Battle from ${date.toLocaleString()}`),
-                  m('a', { class: 'col btn btn-outline-success', href: `battle.html?${BATTLE_ID_PARAM}=${id}` }, 'Resume'),
+                  m('a', { class: 'col-2 btn btn-outline-success', href: `battle.html?${BATTLE_ID_PARAM}=${id}` }, 'Resume'),
                   m(
                     'button',
                     {
-                      class: 'col btn btn-outline-danger',
+                      class: 'col-2 btn btn-outline-danger',
                       onclick: function() {
                         delete battles[id]
                         localStorage.setItem(BATTLE_STORAGE_KEY, JSON.stringify(battles))
